760eaaecd17c9c1b229231bb8adcc683f42ff80f
Nodirbek Bokiev <robertbenn95@gmail.com>
2025-05-20 15:39:14 +0900

    fixed config url

--- config/config.ts
+++ config/config.ts
@@ -3,26 +3,25 @@ const environment =
   (process.env.NODE_ENV as "development" | "test" | "production") ||
   "development";
 
-  const config = {
-    development: {
-      db: process.env.DB,
-      logLevel: "debug",
-      morgan: "dev",
-      origin: ["http://localhost:5173", "http://localhost:5174"], // dev + admin dev
-    },
-    test: {
-      db: "mongodb://localhost/test_db",
-      logLevel: "warn",
-      morgan: "combined",
-      origin: ["http://localhost:5173", "http://localhost:5174"],
-    },
-    production: {
-      db: process.env.DB,
-      logLevel: "error",
-      morgan: "tiny",
-      origin: ["https://study.glasscube.io", "https://admin.glasscube.io"],
-    },
-  };
-  
+const config = {
+  development: {
+    db: process.env.DB,
+    logLevel: "debug",
+    morgan: "dev",
+    origin: ["http://localhost:5173", "http://localhost:5174"], // dev + admin dev
+  },
+  test: {
+    db: "mongodb://localhost/test_db",
+    logLevel: "warn",
+    morgan: "combined",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+  },
+  production: {
+    db: process.env.DB,
+    logLevel: "error",
+    morgan: "tiny",
+    origin: ["https://avalanch.uz"],
+  },
+};
 
 export default config[environment];
